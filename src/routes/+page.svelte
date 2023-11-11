0b3ff7f63c46f7856568397f9f89661607b6b465
alex-k-exe <alexk61@duck.com>
2023-11-11 13:43:52 +1000

    First svelte coding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { compile } from 'svelte/compiler';
+
+	interface Task {
+		description: String;
+		completed: boolean;
+	}
+	let tasks: Task[] = [
+		{ description: 'Feed dog', completed: false },
+		{ description: 'Do homework', completed: true }
+	];
+</script>
+
+<h1>Welcome to MyTodoApp</h1>
+
+{#each tasks as task}
+	<div style="display:inline-flex; gap:10px">
+		<button on:click={() => (task.completed = !task.completed)}>
+			{task.completed ? 'Restart' : 'Complete'}</button
+		>
+		{task.completed ? }
+	</div>
+	<p />
+{/each}
