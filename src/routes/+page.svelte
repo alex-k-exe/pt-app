d21aacefd04c7e1c2fc41fb767a5e335aaa5b3d7
alex-k-exe <alexk61@duck.com>
2023-11-11 20:35:43 +1000

    start basic todo app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,55 @@
 <script lang="ts">
-	import { compile } from 'svelte/compiler';
-
-	interface Task {
+	type Task = {
 		description: String;
-		completed: boolean;
-	}
+		completed?: boolean;
+	};
 	let tasks: Task[] = [
-		{ description: 'Feed dog', completed: false },
+		{ description: 'Feed dog' },
 		{ description: 'Do homework', completed: true }
 	];
+
+	let newTaskDesc: string;
+
+	const createTask = () => {
+		if (!newTaskDesc) return;
+		tasks = [...tasks, { description: newTaskDesc }];
+		newTaskDesc = '';
+	};
 </script>
 
 <h1>Welcome to MyTodoApp</h1>
 
 {#each tasks as task}
-	<div style="display:inline-flex; gap:10px">
-		<button on:click={() => (task.completed = !task.completed)}>
+	<div class="task">
+		<button class="btn" on:click={() => (task.completed = !task.completed)}>
 			{task.completed ? 'Restart' : 'Complete'}</button
 		>
-		{task.completed ? }
+		<p>
+			{#if task.completed}
+				<s>{task.description}</s>
+			{:else}
+				{task.description}
+			{/if}
+		</p>
 	</div>
 	<p />
 {/each}
+
+<div class="task">
+	<button class="btn" on:click={() => createTask()}>Create task</button>
+	<input bind:value={newTaskDesc} />
+</div>
+
+<style lang="scss">
+	.btn {
+		height: fit-content;
+		min-height: 10px;
+		min-width: 50px;
+		padding: 5px;
+	}
+	.task {
+		display: inline-flex;
+		gap: 10px;
+		align-items: center;
+	}
+</style>
