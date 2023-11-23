20181385a0947937bdb95f065a4f2e6e5cfb78d8
alex-k-exe <alexk61@duck.com>
2023-11-23 15:20:45 +1000

    fix thing for fifth time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,34 @@
-<script lang="ts">
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import Counter from './Counter.svelte';
+<script>
+	let hello = 2;
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
 	}
 
-	h1 {
-		width: 100%;
+	:global(body) {
+		font-family: 'Poppins', sans-serif;
+		background-color: #202142;
+		line-height: 1.6;
+		color: #fff;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	:global(ul) {
+		list-style: none;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	:global(a) {
+		color: orange;
+	}
+
+	:global(.container) {
+		max-width: 768px;
+		margin: 100px auto;
+		padding: 0 20px;
 	}
 </style>
