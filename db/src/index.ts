d6996384fdb1655b5e4d2dc594f1b32f3758a409
alex-k-exe <alexk61@duck.com>
2023-12-15 21:40:38 +1000

    typescript going bonky

--- db/src/index.ts
+++ db/src/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/d1';
 import * as schema from './schema';
 import { ChildSchema } from './types';
+var fp = require('lodash/fp');
 
 export interface Env {
 	DB: D1Database;
@@ -12,24 +13,28 @@ export default {
 		switch (pathname) {
 			case '/api/children':
 				const db = drizzle(env.DB, { schema });
-				await db.insert(schema.children).values(await fetchChildrenData());
-				return await db.query.children.findMany();
+
+				// fetch the data for the children
+				// SQL limits insertions to 999 rows at a time
+				const fetchedData = await fp.chunk(fetchChildrenData(), 900);
+				for (const chunk of fetchedData) {
+					await db.insert(schema.children).values(chunk);
+				}
+				return new Response(db.query.children.findMany(), { status: 200 });
 			default:
-				return 'Go to /api/children';
+				return new Response('Go to /api/children', { status: 200 });
 		}
 	},
 };
 
 async function fetchChildrenData() {
 	const fetchedData = await fetch('https://advent.sveltesociety.dev/data/2023/day-three.json');
-	const json = fetchedData.json;
-
 	type JSONChild = {
 		name: string;
 		weight: number;
 	};
+	const jsonChildren = (await fetchedData.json()) as JSONChild[];
 
-	const jsonChildren: JSONChild[] = JSON.parse(json.toString());
 	const formattedChildren: ChildSchema[] = new Array<ChildSchema>(jsonChildren.length);
 	for (let i = 0; i < jsonChildren.length; i++) {
 		formattedChildren[i] = { childName: jsonChildren[i].name, presentsWeight: jsonChildren[i].weight };
