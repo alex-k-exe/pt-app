f47757d3d1ec5e0026e3b33ba3da38714e67e876
alex-k-exe <alexk61@duck.com>
2023-12-18 22:31:45 +1000

    lucia-auth is working!

--- src/routes/signup/+page.server.ts
+++ src/routes/signup/+page.server.ts
@@ -1,5 +1,12 @@
 import { initLucia } from '$lib/server/lucia';
 import { fail, redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.auth.validate();
+	if (session) throw redirect(302, '/');
+	return {};
+};
 
 export const actions: Actions = {
 	default: async ({ request, locals, platform }) => {
