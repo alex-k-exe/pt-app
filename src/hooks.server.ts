599cb4c3d165a585f5e987b41ac1a9c3a0f66e55
alex-k-exe <alexk61@duck.com>
2023-12-19 20:12:26 +1000

    superforms aint working

--- src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import { initLucia } from '$lib/server/lucia';
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = initLucia(event.platform?.env.DB).handleRequest(event);
-	return await resolve(event);
-};
