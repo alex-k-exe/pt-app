d4a929679bfad1422647c495c2b72d441ffdda15
alex-k-exe <alexk61@duck.com>
2023-12-24 13:52:17 +1000

    use wrangler-proxy

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,21 +1,18 @@
 declare global {
 	namespace App {
 		interface Locals {
-			auth: import('lucia').AuthRequest;
+			/* see https://github.com/sveltejs/kit/issues/4292
+			platform.env is undefined in local development with sveltekit
+			in hooks.server.ts:
+			in prod is assigned env.DB, in dev is assigned new DB with wrangler-proxy */
+			DB: D1Database;
 		}
 		interface Platform {
 			env: {
+				// locals.DB should be used instead
 				DB: D1Database;
 			};
 		}
-	}
-
-	namespace Lucia {
-		type Auth = import('$lib/server/lucia').Auth;
-		type DatabaseUserAttributes = {
-			username: string;
-		};
-		type DatabaseSessionAttributes = object;
 	}
 }
 
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
-import { initLucia } from '$lib/server/lucia';
 import { type Handle } from '@sveltejs/kit';
+import { connectD1 } from 'wrangler-proxy';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	event.locals.auth = initLucia(event.platform!.env.DB).handleRequest(event);
+	event.locals.DB = event.platform?.env?.DB ?? connectD1('DB');
 	return await resolve(event);
 };
