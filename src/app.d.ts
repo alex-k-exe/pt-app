7ad941092dea7eeaf93ffa25f0c79f630c019492
alex-k-exe <alexk61@duck.com>
2024-02-11 11:47:01 +1000

    wasting time
    waiting for that sveltekit issue to be solved

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,9 @@
+import { drizzle } from 'drizzle-orm/d1';
+
 declare global {
 	namespace App {
 		interface Locals {
-			/* see https://github.com/sveltejs/kit/issues/4292
-			platform.env is undefined in local development with sveltekit
-			in hooks.server.ts, real or simulated platform is assigned to locals.platform*/
-			platform: Platform;
+			DRIZZLE_DB: ReturnType<typeof drizzle>;
 		}
 		interface Platform {
 			env: {
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,38 +3,58 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import Google from '@auth/sveltekit/providers/google';
 import { type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { drizzle } from 'drizzle-orm/d1';
 import { Miniflare } from 'miniflare';
 
 const platformHandle: Handle = async ({ event, resolve }) => {
-	if (dev) {
-		event.locals.platform = event.platform!;
+	if (event.platform) {
+		event.locals.DRIZZLE_DB = drizzle(event.platform?.env.DB);
 		return await resolve(event);
 	}
+
 	const mf = new Miniflare({
 		script: '',
 		d1Databases: ['DB']
 	});
-	// const platform: App.Platform = {};
-	// platform.env.DB = mf.getD1Database('DB');
+	const db = await mf.getD1Database('DB');
+	event.platform = {
+		env: {
+			DB: db,
+			GOOGLE_ID: process.env.GOOGLE_ID!,
+			GOOGLE_SECRET: process.env.GOOGLE_SECRET!,
+			AUTHJS_SECRET: process.env.AUTHJS_SECRET!
+		},
+		context: {
+			waitUntil: function (): void {
+				throw new Error('Function not implemented.');
+			}
+		},
+		caches: (await mf.getCaches()) satisfies CacheStorage & {
+			default: Cache;
+		}
+	};
 
 	return await resolve(event);
 };
 
-const defaultHandle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event);
-	return response;
-};
+const devAuthHandle = SvelteKitAuth(async () => {
+	return {
+		providers: [
+			Google({ clientId: process.env.GOOGLE_ID, clientSecret: process.env.GOOGLE_SECRET })
+		],
+		secret: process.env.AUTHJS_SECRET,
+		trustHost: true
+	};
+}) satisfies Handle;
 
-const authHandle = dev
-	? defaultHandle
-	: (SvelteKitAuth(async ({ platform }) => {
-			return {
-				providers: [
-					Google({ clientId: platform?.env.GOOGLE_ID, clientSecret: platform?.env.GOOGLE_SECRET })
-				],
-				secret: platform?.env.AUTHJS_SECRET,
-				trustHost: true
-			};
-		}) satisfies Handle);
+const prodAuthHandle = SvelteKitAuth(async ({ platform }) => {
+	return {
+		providers: [
+			Google({ clientId: platform?.env.GOOGLE_ID, clientSecret: platform?.env.GOOGLE_SECRET })
+		],
+		secret: platform?.env.AUTHJS_SECRET,
+		trustHost: true
+	};
+}) satisfies Handle;
 
-export const handle = sequence(platformHandle, authHandle);
+export const handle = sequence(platformHandle, dev ? devAuthHandle : prodAuthHandle);
