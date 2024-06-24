96aa40ade58a65b99ee62952fcd046bf8a753de0
alex-k-exe <alexk61@duck.com>
2024-06-24 12:54:11 +1000

    db token should be wrong

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	dbCredentials: {
 		accountId: 'd882b81f217373f5c5a94ac9513b3859',
 		databaseId: 'dab6f3d7-5933-443d-a617-c645a2b197a5',
-		token: 'dTXAt5v5r7yePVh_n-2dvnmlM0w5A_W-m4Szdqsy'
+		token: 'dTXAt5v5r7yePVh_n--m4Szdqsy'
 	},
 	schema: 'src/lib/drizzleTables.ts',
 	verbose: true,
